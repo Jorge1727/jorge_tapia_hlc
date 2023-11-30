0a2936a4d4e363707c0675599723b5e9fd6698ed
jorge1727 <jorge1727.jt@gmail.com>
2023-11-30 10:06:21 +0100

    primer commit suma a 10

---
+++ main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Haz que la suma sea 10.");
+
+        int n1 = 3;
+        int n2 = 4;
+
+        int reslutado = n1 + n2;
+
+        System.out.println("La suma es " + reslutado);
+    }
+}
